3766398cac38584289e4d27a908c90936eaeadda
Auyy <71812937+Auyy@users.noreply.github.com>
2021-06-08 10:02:05 +0700

    smart_2

--- Smart400/Smart400/Repositories/AppSettingRepository.cs
+++ Smart400/Smart400/Repositories/AppSettingRepository.cs
@@ -28,56 +28,82 @@ namespace Smart400.Repositories
         //    return linesReverse;
         //}
 
-        public IEnumerable<AppSettingModel> Get()
-        {
-            var texts = @"/Users/tangkwa/Desktop/AS400Status/Smart400/Logs/Logs_AS400_Backend_20210606.txt";
-            //var linesReverse = texts.Reverse().ToList();
-            return texts.Select(index => new AppSettingModel
-            {
-                TextSmart = File.ReadAllText(texts)
-            });
-
-        }
-
         //public IEnumerable<AppSettingModel> Get()
         //{
+        //    var texts = @"/Users/tangkwa/Desktop/AS400Status/Smart400/Logs/Logs_AS400_Backend_20210606.txt";
+        //    var linesReverse = texts.Reverse().ToList();
+        //    var lastMsg = linesReverse.FirstOrDefault(m => m.Contains("Wait Order : 5000 mSec"));
 
-        //    var appSettingFullpath = "fileAppSetting.txt";
-        //    var appSetting = ReadFileAppSetting(appSettingFullpath);
-        //    //var curCulture = System.Globalization.CultureInfo.GetCultureInfo("en-US");
-        //    var dateToday = DateTime.Now.ToString("yyyyMMdd");
-        //    var pathFileLog400 = appSetting.LogsAs400Path;
-        //    var filenameLogToday = $"{appSetting.FileNameLogsAs400}{dateToday}.txt";
-        //    var logFile = Path.Combine(pathFileLog400, filenameLogToday);
-
-        //    appSetting.LastUpdate = DateTime.Now;
-        //    var AppSettingJsonStr = JsonSerializer.Serialize(appSetting);
-        //    File.WriteAllText(appSettingFullpath, AppSettingJsonStr);
-        //    //var texts = "fileAppSetting.txt";
-        //    //var linesReverse = texts.Reverse().ToList();
-        //    return logFile.Select(index => new AppSettingModel
+        //    if (texts == null)
         //    {
-        //        TextSmart = File.ReadAllText(logFile)
+
+        //    }
+
+        //    return texts.Select(index => new AppSettingModel
+        //    {
+        //        TextSmart = File.ReadAllText(texts)
         //    });
 
-
         //}
 
-        //private static AppSettingModel ReadFileAppSetting(string filepath)
-        //{
-        //    //read json appsetting
-        //    if (!File.Exists(filepath))
-        //    {
-        //        return null;
-        //    }
-        //    else
-        //    {
-        //        var texts = File.ReadAllText(filepath);
+        public IEnumerable<AppSettingModel> Get()
+        {
+            var appSettingFullpath = "fileAppSetting.txt";
+            var appSetting = ReadFileAppSetting(appSettingFullpath);
+            //var curCulture = System.Globalization.CultureInfo.GetCultureInfo("en-US");
+            var dateToday = DateTime.Now.ToString("yyyyMMdd");
+            var pathFileLog400 = appSetting.LogsAs400Path;
+            var filenameLogToday = $"{appSetting.FileNameLogsAs400}{dateToday}.txt";
+            var logFile = Path.Combine(pathFileLog400, filenameLogToday);
 
-        //        var model = JsonSerializer.Deserialize<AppSettingModel>(texts);
-        //        return model;
-        //    }
-        //}
+            appSetting.LastUpdate = DateTime.Now;
+            var AppSettingJsonStr = JsonSerializer.Serialize(appSetting);
+            File.WriteAllText(appSettingFullpath, AppSettingJsonStr);
+
+                //var lines = File.ReadAllLines(logFile);
+
+                //var linesReverse = lines.Reverse().ToList();
+
+                //var lastMsg = linesReverse.FirstOrDefault(m => m.Contains(appSetting.MessageCheckStatus));
+
+                ////health check service as400
+
+                // var logLines = lastMsg.Split(" : ").ToList();
+                // var logDateStr = logLines.FirstOrDefault();
+                // var logDateTime = DateTime.Parse(logDateStr);
+
+                // //chaeck alert
+                //  var CurDateTime_healthCheck = DateTime.Now;
+
+                //  var diffTime = CurDateTime_healthCheck - logDateTime;
+
+            //if (CurDateTime_healthCheck.Hour % appSetting.HourCheck == 0 && CurDateTime_healthCheck.Minute <= 5)
+            //{
+                
+                
+            //}
+
+            return logFile.Select(index => new AppSettingModel
+            {
+                TextSmart = File.ReadAllText(logFile)
+            });
+        }
+
+        private static AppSettingModel ReadFileAppSetting(string filepath)
+        {
+            //read json appsetting
+            if (!File.Exists(filepath))
+            {
+                return null;
+            }
+            else
+            {
+                var texts = File.ReadAllText(filepath);
+
+                var model = JsonSerializer.Deserialize<AppSettingModel>(texts);
+                return model;
+            }
+        }
 
 
 
