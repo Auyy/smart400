da081fdc688bba7767c5004be8ce10f83765c356
Auyy <71812937+Auyy@users.noreply.github.com>
2021-06-18 10:43:48 +0700

    smart 400

--- Smart400/Smart400/Services/As400Service.cs
+++ Smart400/Smart400/Services/As400Service.cs
@@ -1,36 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
-using System.Configuration;
 using System.IO;
 using System.Linq;
-<<<<<<< HEAD:Smart400/Smart400/Services/As400Service.cs
-=======
-using System.Text.Json;
-using System.Threading;
-using Dapper;
-using Microsoft.AspNetCore.Mvc;
->>>>>>> 91434146b6931377e8c66cace70b249a95efc6d6:Smart400/Smart400/Repositories/As400Repository.cs
-=======
-using System.Linq;
->>>>>>> 91434146b6931377e8c66cace70b249a95efc6d6
 using Microsoft.Extensions.Configuration;
 using Smart400.Models;
 using Smart400.Repositories;
 
 namespace Smart400.Services
 {
-<<<<<<< HEAD
-<<<<<<< HEAD:Smart400/Smart400/Services/As400Service.cs
     public interface IAs400Service
-=======
-    public interface IAs400Repository
-    {
-        As400Response Get();
-    }
-
-    public class As400Repository : IAs400Repository
->>>>>>> 91434146b6931377e8c66cace70b249a95efc6d6:Smart400/Smart400/Repositories/As400Repository.cs
     {
         As400Response GetStatus();
     }
@@ -40,7 +18,6 @@ namespace Smart400.Services
         private readonly IAs400Repository as400Repository;
         private IConfiguration configuration;
 
-<<<<<<< HEAD:Smart400/Smart400/Services/As400Service.cs
         public As400Service(IAs400Repository as400Repository,IConfiguration configuration)
         {
             this.as400Repository = as400Repository;
@@ -49,23 +26,10 @@ namespace Smart400.Services
         }
 
         public As400Response GetStatus()
-=======
-        private IConfiguration configuration;
-        public As400Repository(IConfiguration configuration)
-        {
-            this.configuration = configuration;
-        }
-
-        public As400Response Get()
->>>>>>> 91434146b6931377e8c66cace70b249a95efc6d6:Smart400/Smart400/Repositories/As400Repository.cs
         {
             //var curCulture = System.Globalization.CultureInfo.GetCultureInfo("en-US");
 
             var dateToday = DateTime.Now.ToString("yyyyMMdd");
-<<<<<<< HEAD:Smart400/Smart400/Services/As400Service.cs
-=======
-
->>>>>>> 91434146b6931377e8c66cace70b249a95efc6d6:Smart400/Smart400/Repositories/As400Repository.cs
             var pathFileLog400 = configuration.GetSection("ModelSetting:LogsAs400Path").Value;
             var fileNameLogs = configuration.GetSection("ModelSetting:FileNameLogsAs400").Value;
             var filenameLogToday = $"{fileNameLogs}{dateToday}.txt";
@@ -136,18 +100,13 @@ namespace Smart400.Services
 
                     if (diffTime.Days == 0 && diffTime.Hours == 0 && diffTime.Minutes <= timerMinuteCheck)
                     {
-<<<<<<< HEAD:Smart400/Smart400/Services/As400Service.cs
                         return RES_SERVER_RUN;
-=======
-                            return RES_SERVER_RUN;
->>>>>>> 91434146b6931377e8c66cace70b249a95efc6d6:Smart400/Smart400/Repositories/As400Repository.cs
                     }
                     else
                     {
                         return RES_SERVER_DOWN;
                     }
                 }
-<<<<<<< HEAD:Smart400/Smart400/Services/As400Service.cs
                 else
                 {
                     return RES_SERVER_DOWN2;
@@ -156,43 +115,10 @@ namespace Smart400.Services
             else
             {
                 return RES_SERVER_DOWN3;
-=======
-                 else
-                 {
-                    return RES_SERVER_DOWN2;
-                 }
-            }
-            else
-            {
-                return RES_SERVER_DOWN3; 
->>>>>>> 91434146b6931377e8c66cace70b249a95efc6d6:Smart400/Smart400/Repositories/As400Repository.cs
             }
 
         }
 
 
-=======
-    public interface IAs400Service
-    {
-        As400Response GetStatus();
-    }
-    public class As400Service : IAs400Service
-    {
-        private readonly IAs400Repository as400Repository;
-
-        public As400Service(IAs400Repository as400Repository)
-        {
-            this.as400Repository = as400Repository;
-        }
-
-        public As400Response GetStatus()
-        {
-            var appsetting = as400Repository.Get();
-
-            return appsetting;
-        }
-
-       
->>>>>>> 91434146b6931377e8c66cace70b249a95efc6d6
     }
 }
